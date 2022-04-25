591d853bc9169cefe7a472158cd2fcf5f20ea959
John Cortés <44423117+johncortes00@users.noreply.github.com>
2022-04-25 17:13:47 -0500

    Updates

---
+++ precache-manifest.9152a8717ecc927bbfc24a593830caa3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7f45e26bf9729a67edc",
+    "url": "/robofriends/static/css/main.a6595b37.chunk.css"
+  },
+  {
+    "revision": "c7f45e26bf9729a67edc",
+    "url": "/robofriends/static/js/main.c7f45e26.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robofriends/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "03f7f0c60a07ebb74932",
+    "url": "/robofriends/static/css/2.81cdd51c.chunk.css"
+  },
+  {
+    "revision": "03f7f0c60a07ebb74932",
+    "url": "/robofriends/static/js/2.03f7f0c6.chunk.js"
+  },
+  {
+    "revision": "5a6fc926011dbcb5a003",
+    "url": "/robofriends/static/js/runtime~main.5a6fc926.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "553e09e06b8560544dffc2ced01f46d3",
+    "url": "/robofriends/index.html"
+  }
+];
